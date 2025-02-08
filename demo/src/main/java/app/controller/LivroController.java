99b8df9ac1dd86a8cce323286167d8a3153d62c1
CassianoE <kssycs@outlook.com>
2025-02-07 22:07:47 -0300

    atulizando endpoints

--- demo/src/main/java/app/controller/LivroController.java
+++ demo/src/main/java/app/controller/LivroController.java
@@ -16,7 +16,7 @@ public class LivroController {
     @Autowired
     private LivroService livroService;
 
-    @RequestMapping("save")
+    @RequestMapping("/save")
     public ResponseEntity<String> save(@RequestBody Livro livro){
         try {
             String mensagem = this.livroService.save(livro);
